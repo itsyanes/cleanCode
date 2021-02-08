361addd8145efb7f0c987ee828d90205f644a907
Yanis TOUAHRI <touahriyanis@gmail.com>
2021-02-08 11:39:37 +0100

    initialized structure of the project

---
+++ src/ExamCleanCode.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamCleanCode.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/ExamCleanCode.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamCleanCode.Core
+{
+    public class Class1
+    {
+    }
+}
